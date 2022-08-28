835b555d47385fe7a47aecfe3389fa23e1762a49
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-28 17:05:26 -0400

    Created Feed component

---
+++ my-project/components/Feed.js
@@ -0,0 +1,35 @@
+function Feed() {
+    return (
+        <div>
+
+            <h1>If the NHTSA is investigating Tesla’s Autopilot use around motorcycles, as we reported it was just days ago, they may want to hurry up.
+
+That’s because yet another motorcyclist has died as a result of a an accident with a Tesla, this time in Boca Raton, Florida. It is unclear whether or not Autopilot was involved in the accident.
+
+51-year-old biker Ingrid Eva Noon was traveling west on Southwest 18th Street on her Kawasaki Vulcan, according to the South Florida Sun Sentinel, when, shortly after 2AM, a driver in a Tesla “struck her motorcycle from behind”.
+
+The Sheriff’s office did confirm that the driver “had drugs or alcohol in his system at the time of the crash” and it isn’t yet clear whether or not the vehicle was on Autopilot at the time of the accident. An investigation is ongoing.
+
+However, the incident is eeriliy similar to other incidents the NHTSA is already investigating wherein Teslas on Autopilot potentially fail to recognize motorcycles in front of them. In the incidents the NHTSA is looked at, “Teslas collided with motorcycles on freeways in the darkness.”
+
+Recall, about 20 days ago we wrote that the NHTSA was investigating potential Autopilot crashes that left two motorcyclists dead.
+
+The Teslas were “apparently running on Autopilot”, according to APs coverage of the story. The accidents wound up killing 2 motorcyclists, the report says. The NHTSA is looking at whether or not Tesla vehicle automation stops the vehicles for motorcycles.
+
+Both accidents were similar in nature: the NHTSA said it “sent investigation teams to two crashes last month in which Teslas collided with motorcycles on freeways in the darkness”. In both instances, the motorcyclists were killed.
+
+The agency now has suspicions “that Tesla’s partially automated driver-assist system was in use” during both accidents.
+
+The first accident was at 4:47am, July 7 on State Route 91, on a freeway in Riverside, California, the report says. A Model Y collided with a green Yamaha V-Star motorcycle that was ahead of it and the driver of the bike was ejected from his motorcycle.
+
+Another crash happened at 1:09am on July 24, on Interstate 15 near Draper, Utah. A Model 3 was behind a Harley Davidson, the Utah Department of Public Safety said.
+
+“The driver of the Tesla did not see the motorcyclist and collided with the back of the motorcycle, which threw the rider from the bike,” the statement says. The rider of the Harley was pronounced dead at the scene. The driver told authorities he had Autopilot on, the report says.</h1>
+
+            {/* Articles */}
+
+        </div>
+    )
+}
+
+export default Feed
