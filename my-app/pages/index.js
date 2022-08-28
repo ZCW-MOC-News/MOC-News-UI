b62fc936aedfb5f3865d9d6beac35fd5e611441c
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-28 08:49:47 -0400

    First commit, installed next.js

---
+++ my-app/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+// import styles from '../styles/Home.module.css'
+
+/* Index.js is the Homepage */
+
+export default function Home() {
+  return (
+    // <div className={styles.container}>
+    <div>
+      <Head>
+        <title>MOC News</title>
+      
+      </Head>
+
+      <h1>Let's build MOC News</h1>
+    </div>
+  )
+}
