39348faa1326fa1e1f5626767d0f24913ece51d0
Meilin Li (Linda) <limeilinda@gmail.com>
2022-08-28 12:34:33 -0400

    Added Heroicons and implemented icons into Header

--- my-project/components/Header.js
+++ my-project/components/Header.js
@@ -17,18 +17,25 @@ function Header() {
     return (
         <div>
 
-            <h1>Header</h1>
+            <h1 className="text-6xl">MOC News</h1>
 
             { /* Header Left */ }
-            <div>
-                <Image src="https://links.papareact.com/5me" alt="logo"
-                width={40}
-                height={40} 
-                layout="fixed"
+            <div className="flex items-center">
+                <Image 
+                    src="https://links.papareact.com/5me"
+                    width={40}
+                    height={40} 
+                    layout="fixed"
                 />
-                <div>
+                <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
 
-                    <input type="text" placeholder='Search News Articles' />
+                    <SearchIcon className="h-6 text-grey-600" />
+
+                    <input 
+                        className="flex m1-2 items-center bg-transparent outline-none
+                        placeholder-gray-500" 
+                        type="text" 
+                        placeholder="Search News" />
 
                 </div>
 
