e73efc188355156459647ecb1e3612e084b1604a
Chris Kent <Chris_Kent@ymail.com>
2022-08-31 15:50:16 -0400

    Sidebar Visible

---
+++ my-project/components/Sidebar.js
@@ -0,0 +1,35 @@
+// import { useSession } from "next-auth/client";
+import SidebarRow from "./SidebarRow";
+
+import {
+    ChipIcon,
+    FilmIcon,
+    OfficeBuildingIcon,
+    BeakerIcon,
+    SpeakerphoneIcon,
+    EyeIcon,
+    GlobeAltIcon,
+} from "@heroicons/react/outline";
+
+
+
+function Sidebar() {
+    // const [session, loading] = useSession();
+
+    return (
+        <div className="p-22 mt-5 max-w-[600px] xl:min-w- [300px]">
+            {/* <SidebarRow src={session.user.image} title={session.user.name} /> */}
+            <SidebarRow Icon={GlobeAltIcon} title="General" />
+            <SidebarRow Icon={OfficeBuildingIcon} title="Business" />
+            <SidebarRow Icon={FilmIcon} title="Entertainment" />
+            <SidebarRow Icon={EyeIcon} title="Health" />
+            <SidebarRow Icon={BeakerIcon} title="Science" />
+            <SidebarRow Icon={SpeakerphoneIcon} title="Sports" />
+            <SidebarRow Icon={ChipIcon} title="Technology" />
+
+        </div>
+    );
+}
+
+
+export default Sidebar
