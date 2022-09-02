2f2ee051c66e149d0f85cd0c7715d3efceeba3f3
octavia <Ogomes21@gmail.com>
2022-09-02 09:46:54 -0400

    Fixed yarn run issues and deleted node modules.

--- my-project/components/Header.js
+++ my-project/components/Header.js
@@ -2,11 +2,13 @@ import Image from "next/image";
 import AppleNewsLogo from "./AppleNewsLogo.svg"
 import Dropdown from "./Dropdown.js"
 
+
+
 import {
     BellIcon,
     ChatIcon,
     HomeIcon,
-    UserCircleIcon,
+    // UserCircleIcon,
     UserGroupIcon,
     ViewGridIcon,
 } from "@heroicons/react/solid";
@@ -17,6 +19,7 @@ import {
     ShoppingCartIcon,
 } from "@heroicons/react/outline";
 import HeaderIcon from "./HeaderIcon";
+
 
 function Header() {
     
@@ -33,8 +36,6 @@ function Header() {
                     height={40} 
                     layout="fixed"
                 />
-
-                
 
                 <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
 
@@ -77,17 +78,20 @@ function Header() {
                 <ViewGridIcon className="icon" />
                 <ChatIcon className="icon" />
                 <BellIcon className="icon" />
-                <Dropdown/>                        
-            
-
-
+                <Dropdown/>
+                
+                
+                        
             </div>       
+            
         </div>
-
+        
+    
         
         
     );
 }
+
 
 export default Header;
 
