1001a4241c0ec2f6b3a0eae11a18dd819b579bc8
Meilin Li (Linda) <limeilinda@gmail.com>
2022-09-02 23:41:55 -0400

    Modified Feed.js to format the articles

--- my-project/components/Feed.js
+++ my-project/components/Feed.js
@@ -1,6 +1,26 @@
 import React, { useState } from 'react';
 import axios from 'axios';
 import Stories from './Stories';
+import Story from "./Story";
+import { motion } from "framer-motion";
+
+const list = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: { staggerChildren: 0.2, deplay: 1 },
+  },
+};
+const item = {
+  hidden: { x: -20, opacity: 0 },
+  show: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
 
 
 function Feed() {
@@ -21,64 +41,48 @@ function Feed() {
 
     React.useEffect(() => getArticles(), []);
 
-    return (
+    // return (
 
         
-        // <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
-        //     <div className='mx-auto max-wd-md md:max-w-1g'>
+    //     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+    //         <div className='mx-auto max-wd-md md:max-w-1g'>
       
-        //         <div><Stories />
-        //         <div class="flex-grow grid grid-cols-2 gap-10 place-content-center h-48">
-        //             {articles
-        //                 &&
-        //                 articles.map((item, i) => (
-        //                     <div key={i}>
+    //             <div><Stories />
+    //             <div class="flex-grow grid grid-cols-1 gap-10 place-content-center h-48">
+    //                 {articles
+    //                     &&
+    //                     articles.map((item, i) => (
+    //                         <div key={i}>
                                 
-        //                         <div className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">{item.title}</div>
-        //                             <div>Author: {item.author}</div>
-        //                             <div> Description: {item.description}</div>
+    //                             <div className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">{item.title}</div>
+    //                                 <div>Author: {item.author}</div>
+    //                                 <div> Description: {item.description}</div>
 
-        //                     </div>
+    //                         </div>
 
-        //                 ))
-        //             }
-        //         </div>
-        //         </div>
-        //     </div>
-        // </div>
-        <Link href={`/items/${story.id}`}>
-        <div className="news-card flex flex-row font-inter">
-          <div className="px-5 hidden lg:flex flex-col justify-center text-center w-20">
-            <UpIcon />
-            <p className="text-gray-600 text-xs font-medium">{story.score}</p>
-          </div>
-          <div className="px-5">
-            <h2 className="font-medium text-sm text-soft-black">{story.title}</h2>
-            <div className="flex mt-2">
-              <p className="hidden lg:inline-block text-xs mr-4 text-gray-600">
-                by <span className="text-red-500 font-medium">{story.user}</span>
-              </p>
-              <p className="text-xs text-gray-600 mr-4"> {story.time}</p>
-              <p className="text-xs hidden lg:inline-block text-gray-600 mr-4">
-                {getHost(story.source, story.id)}
-              </p>
-              <figure className="flex items-start mr-4">
-                <ChatIcon />
-                <figcaption className="text-xs text-gray-600">
-                  {story.comment_count}
-                </figcaption>
-              </figure>
-  
-              <div className="lg:hidden flex items-start">
-                <ThumbsUp />
-                <p className="text-xs text-gray-600">{story.score}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </Link>
+    //                     ))
+    //                 }
+    //             </div>
+    //             </div>
+    //         </div>
+    //     </div>
+    // );
+    return (
+      <div className="mt-4">
+        <motion.div
+          className="children"
+          initial="hidden"
+          animate="show"
+          variants={list}
+        >
+          {articles.map((story, i) => (
+            <motion.div variants={item}>
+              <Story story={story} key={i} />
+            </motion.div>
+          ))}
+        </motion.div>
+      </div>
     );
-    
 }
 
 
---
+++ my-project/components/Story.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import Link from "next/link";
+import ChatIcon from "../components/icons/chat";
+import UpIcon from "../components/icons/up";
+import ThumbsUp from "../components/icons/thumbsup";
+
+export default function Story({ story }) {
+  return (
+    // <Link href={`/items/${story.id}`}>
+      <div className="news-card flex flex-row font-inter">
+        <div className="px-5 hidden lg:flex flex-col justify-center text-center w-20">
+          <UpIcon />
+          <p className="text-gray-600 text-xs font-medium">5</p>
+        </div>
+        <div className="px-5">
+          <h2 className="font-medium text-sm text-soft-black">{story.title}</h2>
+          <div className="flex mt-2">
+            <p className="hidden lg:inline-block text-xs mr-4 text-gray-600">
+              by <span className="text-red-500 font-medium">{story.author}</span>
+            </p>
+            <p className="text-xs text-gray-600 mr-4"> {story.date}</p>
+            <p className="text-xs hidden lg:inline-block text-gray-600 mr-4">
+              {story.source}
+            </p>
+            <figure className="flex items-start mr-4">
+              <ChatIcon />
+              <figcaption className="text-xs text-gray-600">
+                {/* {story.comment_count} */}
+                10
+              </figcaption>
+            </figure>
+
+            <div className="lg:hidden flex items-start">
+              <ThumbsUp />
+              <p className="text-xs text-gray-600">100</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    // </Link>
+  );
+}
