b7554098eb3bd8346fd7f43c5a50218d629bcaea
Meilin Li (Linda) <limeilinda@gmail.com>
2022-09-02 22:04:16 -0400

    Added icon folder and imported icons

--- my-project/components/Feed.js
+++ my-project/components/Feed.js
@@ -1,11 +1,5 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Stories from './Stories';
 
 
@@ -29,39 +23,62 @@ function Feed() {
 
     return (
 
+        
+        // <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+        //     <div className='mx-auto max-wd-md md:max-w-1g'>
+      
+        //         <div><Stories />
+        //         <div class="flex-grow grid grid-cols-2 gap-10 place-content-center h-48">
+        //             {articles
+        //                 &&
+        //                 articles.map((item, i) => (
+        //                     <div key={i}>
+                                
+        //                         <div className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">{item.title}</div>
+        //                             <div>Author: {item.author}</div>
+        //                             <div> Description: {item.description}</div>
 
-        <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
-            <div className='mx-auto max-wd-md md:max-w-1g'>
-                <div><Stories />
-                    {articles
-                        &&
-                        articles.map((item, i) => (
-                            <div key={i}>
-                                <TableContainer component={Paper}>
-                                    <TableHead>
-                                        <TableRow>
-                                            <TableCell>Title: {item.title}</TableCell>
+        //                     </div>
 
-                                        </TableRow>
-                                        <TableRow>
-
-                                            <TableCell>Author: {item.author}</TableCell>
-                                        </TableRow>
-                                        <TableRow>
-
-                                            <TableCell>Description: {item.description}</TableCell>
-                                        </TableRow>
-                                    </TableHead>
-                                </TableContainer>
-
-                            </div>
-
-                        ))
-                    }
-                </div>
+        //                 ))
+        //             }
+        //         </div>
+        //         </div>
+        //     </div>
+        // </div>
+        <Link href={`/items/${story.id}`}>
+        <div className="news-card flex flex-row font-inter">
+          <div className="px-5 hidden lg:flex flex-col justify-center text-center w-20">
+            <UpIcon />
+            <p className="text-gray-600 text-xs font-medium">{story.score}</p>
+          </div>
+          <div className="px-5">
+            <h2 className="font-medium text-sm text-soft-black">{story.title}</h2>
+            <div className="flex mt-2">
+              <p className="hidden lg:inline-block text-xs mr-4 text-gray-600">
+                by <span className="text-red-500 font-medium">{story.user}</span>
+              </p>
+              <p className="text-xs text-gray-600 mr-4"> {story.time}</p>
+              <p className="text-xs hidden lg:inline-block text-gray-600 mr-4">
+                {getHost(story.source, story.id)}
+              </p>
+              <figure className="flex items-start mr-4">
+                <ChatIcon />
+                <figcaption className="text-xs text-gray-600">
+                  {story.comment_count}
+                </figcaption>
+              </figure>
+  
+              <div className="lg:hidden flex items-start">
+                <ThumbsUp />
+                <p className="text-xs text-gray-600">{story.score}</p>
+              </div>
             </div>
+          </div>
         </div>
+      </Link>
     );
+    
 }
 
 
