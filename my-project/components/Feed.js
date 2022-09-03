ce2bd053902eed8788cb289d74746ccb66b730fc
Meilin Li (Linda) <limeilinda@gmail.com>
2022-09-03 00:08:33 -0400

    Centered the articles

--- my-project/components/Feed.js
+++ my-project/components/Feed.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import Stories from './Stories';
+import React, { useState } from "react";
+import axios from "axios";
+import Stories from "./Stories";
 import Story from "./Story";
 import { motion } from "framer-motion";
+import Head from "next/head";
 
 const list = {
   hidden: { opacity: 0 },
@@ -22,68 +23,78 @@ const item = {
   },
 };
 
-
 function Feed() {
+  const [articles, setArticles] = useState([]);
 
-    const [articles, setArticles] = useState([]);
+  const getArticles = () => {
+    axios
+      .get("http://localhost:8080/articles/all")
+      .then((response) => {
+        console.log("Success");
+        setArticles(response.data);
+      })
+      .catch((e) => {
+        console.log("Error");
+        setArticles([]);
+      });
+  };
 
-    const getArticles = () => {
-        axios.get("http://localhost:8080/articles/all")
-            .then((response) => {
-                console.log("Success");
-                setArticles(response.data);
-            })
-            .catch((e) => {
-                console.log("Error");
-                setArticles([]);
-            });
-    };
+  React.useEffect(() => getArticles(), []);
 
-    React.useEffect(() => getArticles(), []);
+  // return (
 
-    // return (
+  //     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
+  //         <div className='mx-auto max-wd-md md:max-w-1g'>
 
-        
-    //     <div className="flex-grow h-screen pb-44 pt-6 mr-4 xl:mr-40 overflow-y-auto">
-    //         <div className='mx-auto max-wd-md md:max-w-1g'>
-      
-    //             <div><Stories />
-    //             <div class="flex-grow grid grid-cols-1 gap-10 place-content-center h-48">
-    //                 {articles
-    //                     &&
-    //                     articles.map((item, i) => (
-    //                         <div key={i}>
-                                
-    //                             <div className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">{item.title}</div>
-    //                                 <div>Author: {item.author}</div>
-    //                                 <div> Description: {item.description}</div>
+  //             <div><Stories />
+  //             <div class="flex-grow grid grid-cols-1 gap-10 place-content-center h-48">
+  //                 {articles
+  //                     &&
+  //                     articles.map((item, i) => (
+  //                         <div key={i}>
 
-    //                         </div>
+  //                             <div className="box-decoration-slice bg-gradient-to-r from-indigo-600 to-pink-500 text-white px-2 ...">{item.title}</div>
+  //                                 <div>Author: {item.author}</div>
+  //                                 <div> Description: {item.description}</div>
 
-    //                     ))
-    //                 }
-    //             </div>
-    //             </div>
-    //         </div>
-    //     </div>
-    // );
-    return (
-      <div className="mt-4">
-        <motion.div
-          className="children"
-          initial="hidden"
-          animate="show"
-          variants={list}
-        >
-          {articles.map((story, i) => (
-            <motion.div variants={item}>
-              <Story story={story} key={i} />
+  //                         </div>
+
+  //                     ))
+  //                 }
+  //             </div>
+  //             </div>
+  //         </div>
+  //     </div>
+  // );
+  return (
+    <>
+      <Head>
+        <title>MOC News</title>
+      </Head>
+      <div className="flex justify-center space-x-3 mx-auto">
+        <div className="lg:col-span-2 mt-8">
+          <span className="main-title flex items-center text-soft-black">
+            <h1 className="fancy-undeline">Recent Articles</h1>
+          </span>
+          <div className="mt-4">
+            <motion.div
+              className="children"
+              initial="hidden"
+              animate="show"
+              variants={list}
+            >
+              {articles.map((story, i) => (
+                <motion.div variants={item}>
+                  <Story story={story} key={i} />
+                </motion.div>
+              ))}
             </motion.div>
-          ))}
-        </motion.div>
+          </div>
+          <button className="more-btn">Load more</button>
+        </div>
       </div>
-    );
+    </>
+  );
 }
-
 
 export default Feed;
--- my-project/pages/index.js
+++ my-project/pages/index.js
@@ -1,7 +1,7 @@
-import Head from 'next/head';
-import Header from '../components/Header';
-import Feed from '../components/Feed';
-import Sidebar from '../components/Sidebar';
+import Head from "next/head";
+import Header from "../components/Header";
+import Feed from "../components/Feed";
+import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
@@ -12,21 +12,16 @@ export default function Home() {
 
       {/* <h1>Let's build MOC News!</h1> */}
 
-      { /* Header */ }
+      {/* Header */}
       <Header />
 
       <main className="flex">
-
-      { /* Sidebar */ }
-      <Sidebar />
-      { /* Newsfeed */ }
-      <Feed />
-      { /* Widgets */ }
-
+        {/* Sidebar */}
+        <Sidebar />
+        {/* Newsfeed */}
+        <Feed />
+        {/* Widgets */}
       </main>
-
     </div>
-  )
+  );
 }
-
-
