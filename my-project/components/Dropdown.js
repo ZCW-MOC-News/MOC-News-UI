10d7242c8b51c7822fcd126e590c67ef5ed6125f
Meilin (Linda) Li <106841467+limeilinda@users.noreply.github.com>
2022-09-05 22:36:11 -0400

    sign up page

--- my-project/components/Dropdown.js
+++ my-project/components/Dropdown.js
@@ -64,7 +64,7 @@ export default function Example() {
             <Menu.Item>
               {({ active }) => (
                 <a
-                  href="#"
+                  href="/signup"
                   className={classNames(
                     active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
                     'block px-4 py-2 text-sm'
